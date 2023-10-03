2c41f4af67ff46719fda5aea6cc853aca4af9f5a
Sabeer Narula <sabeernarula47@gmail.com>
2023-10-03 08:34:06 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, request, jsonify, session
+import random
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+
+# Example questions with answers and data
+questions = [
+    # {"text": "Who has more followers on Instagram?", "options": ["Obama", "Trump"], "answer": "Obama", "data": {"Obama": "130M", "Trump": "90M"}},
+    # {"text": "Which country has a higher population?", "options": ["North Korea", "Finland"], "answer": "North Korea", "data": {"North Korea": "25M", "Finland": "5.5M"}},
+    # {"text": "Which animal is heavier?", "options": ["Elephant", "Blue Whale"], "answer": "Blue Whale", "data": {"Elephant": "14,000 lbs", "Blue Whale": "200,000 lbs"}},
+    # {"text": "Which planet is larger by surface area?", "options": ["Earth", "Mars"], "answer": "Earth", "data": {"Earth": "12,742 km", "Mars": "6,779 km"}},
+    # {"text": "Which animal lives longer?", "options": ["Dog", "Parrot"], "answer": "Parrot", "data": {"Dog": "10-13 years", "Parrot": "50-100 years"}},
+    # {"text": "Which state has a higher population?", "options": ["California", "Texas"], "answer": "California", "data": {"California": "39.5M", "Texas": "29M"}},
+    # {"text": "Which statue is taller?", "options": ["Statue of Liberty", "Christ the Redeemer"], "answer": "Statue of Liberty", "data": {"Statue of Liberty": "151 ft", "Christ the Redeemer": "98 ft"}},
+    # {"text": "Which has more monthly Spotify listeners?", "options": ["Drake", "Ed Sheeran"], "answer": "Drake", "data": {"Drake": "65M", "Ed Sheeran": "60M"}},
+    # {"text": "Which celebrity is worth more?", "options": ["Elon Musk", "Jeff Bezos"], "answer": "Elon Musk", "data": {"Elon Musk": "$300B", "Jeff Bezos": "$190B"}},
+    # {"text": "Which country has a larger area?", "options": ["USA", "Canada"], "answer": "Canada", "data": {"USA": "9.8M km²", "Canada": "9.9M km²"}},
+    {"text": "Which element has a higher atomic number?", "options": ["Hydrogen", "Helium"], "answer": "Helium", "data": {"Hydrogen": "1", "Helium": "2"}},
+    {"text": "Which ocean is deeper?", "options": ["Atlantic", "Pacific"], "answer": "Pacific", "data": {"Atlantic": "8,376 m", "Pacific": "10,984 m"}},
+    {"text": "Which planet is closer to the sun?", "options": ["Venus", "Mars"], "answer": "Venus", "data": {"Venus": "0.72 AU", "Mars": "1.52 AU"}}
+
+]
+
+@app.route('/')
+def index():
+    if 'questions_answered' not in session:
+        session['questions_answered'] = []
+    if 'streak' not in session:
+        session['streak'] = 0 
+    if 'score' not in session:
+        session['score'] = 0
+
+    remaining_questions = [q for i, q in enumerate(questions) if i not in session['questions_answered']]
+    if not remaining_questions:
+        return render_template('game_over.html', streak=session['streak'], score=session['score'])
+
+    random_index = random.choice(range(len(remaining_questions)))
+    question = remaining_questions[random_index]
+    session['questions_answered'].append(questions.index(question))
+
+    session.modified = True  # Ensure the session knows it has been modified
+    current_streak = session.get('streak', 0)  # Get current streak from session
+    final_score = session.get('score', 0) 
+    return render_template('index.html', question=question, current_streak=current_streak, score=final_score)
+
+@app.route('/reset_streak', methods=['POST'])
+def reset_streak():
+    session['streak'] = 0
+    return jsonify({"status": "success"})
+
+@app.route('/reset', methods=['POST'])
+def reset_game():
+    session.pop('questions_answered', None)
+    session['streak'] = 0 
+    session['score'] = 0
+    return jsonify({"status": "success"})
+
+@app.route('/check', methods=['POST'])
+def check_answer():
+    user_answer = request.json['answer']
+    question_text = request.json['question']
+    question = next(q for q in questions if q['text'] == question_text)
+    user_score = request.json.get('score', 0)
+
+    is_correct = user_answer == question['answer']
+    if is_correct:
+        session['streak'] += 1
+        session['score'] = user_score
+    else:
+        session['streak'] = 0
+    return jsonify({
+        "correct": is_correct,
+        "data": question['data'],
+        "streak": session['streak'],
+        "score": session['score']
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
